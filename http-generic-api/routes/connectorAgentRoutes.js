8c7e585db5d4be0b2d9b6cf2ac1c38baa1212f0e
Growth Intelligence Platform Admin Assistant <mad4b.digital@gmail.com>
2026-05-18 20:27:30 +0300

    Revert "Add public connector installer route on connector agent surface"
    This reverts commit 691600ec0e3d35f87f056e6f55b50e7bf9a651c7.

--- http-generic-api/routes/connectorAgentRoutes.js
+++ http-generic-api/routes/connectorAgentRoutes.js
@@ -2,13 +2,6 @@ import { Router } from "express";
 import crypto from "node:crypto";
 import { readFile } from "node:fs/promises";
 import path from "node:path";
-import { getPool } from "../db.js";
-import {
-  verifyInstallerDownloadToken,
-  buildInstallPowerShell,
-  DEFAULT_WINDOWS_ALIASES,
-  CONNECTOR_PORT,
-} from "./localConnectorInstallRoutes.js";
 
 const AGENT_VERSION = "2026.05.18.1";
 const ROOT = process.cwd();
